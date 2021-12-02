a912f521afe8183ff976bba874bb82f78a14434d
MukaiLab <38516309+MukaiLab@users.noreply.github.com>
2021-12-02 14:54:53 +0900

    Add files via upload

---
+++ TestGet.py
@@ -0,0 +1,15 @@
+import SensorDataOperation
+
+
+def main():
+  dataType = 'Test'
+  deviceNumber = '001'
+
+  sdo = SensorDataOperation.SensorDataOperation(dataType=dataType, deviceNumber=deviceNumber)
+
+  response = sdo.get()
+  print(response)
+  print(response.json())
+
+if __name__ == "__main__":
+  main()
