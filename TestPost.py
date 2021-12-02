638c54fc4721c23babd148851c205f81e78e590e
MukaiLab <38516309+MukaiLab@users.noreply.github.com>
2021-12-02 14:41:01 +0900

    使用例

---
+++ TestPost.py
@@ -0,0 +1,37 @@
+import datetime, SensorDataOperation
+
+
+def main():
+  nowtime = str(datetime.datetime.now())
+  dataType = 'Test'
+  deviceNumber = '001'
+
+  sdo = SensorDataOperation.SensorDataOperation(dataType=dataType, deviceNumber=deviceNumber)
+
+  postData = {
+    "id": sdo.dataId,
+    "type": dataType,
+    "dateissued": {
+      "type": "TimeDate",
+      "value": nowtime
+    },
+    "location": {
+      "type": "geo:json",
+      "value": {
+        "type": "Point",
+        "coordinates": [136.6285,36.5313]
+      }
+    },
+    "name": {
+        "type": "Text",
+        "value": "Test"
+    }
+  }
+  
+  print(postData)
+  response = sdo.post(postData=postData)
+  print(response)
+  sdo.delete()
+
+if __name__ == "__main__":
+  main()
